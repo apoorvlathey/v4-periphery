a6feed7a3fa88474e30164993eee5d343b43ad30
apoorvlathey <apoorv@eth.sh>
2026-09-05 09:22:02 +0400

    docs: record V4QuoterV2 mainnet deployment

--- script/DeployV4QuoterV2.s.sol
+++ script/DeployV4QuoterV2.s.sol
@@ -12,7 +12,12 @@ contract DeployV4QuoterV2 is Script {
     function run(address poolManager) public returns (IV4QuoterV2 state) {
         vm.startBroadcast();
 
-        // forge script --broadcast --sig 'run(address)' --rpc-url <RPC_URL> --private-key <PRIV_KEY> --verify script/DeployV4QuoterV2.s.sol:DeployV4QuoterV2 <POOL_MANAGER_ADDR>
+        // export WALLETCHAN_RPC_URL=http://127.0.0.1:4210
+        // export WALLETCHAN_SENDER=$(cast rpc --rpc-url "$WALLETCHAN_RPC_URL" eth_accounts | jq -er '.[0]')
+        // forge script script/DeployV4QuoterV2.s.sol:DeployV4QuoterV2 \
+        //   --sig 'run(address)' 0x000000000004444c5dc75cB358380D2e3dE08A90 \
+        //   --rpc-url "$WALLETCHAN_RPC_URL" --broadcast --unlocked --slow \
+        //   --sender "$WALLETCHAN_SENDER" --verify -vvvv
         state = Deploy.v4QuoterV2(poolManager, hex"00");
         console2.log("V4QuoterV2", address(state));
         console2.log("PoolManager", address(state.poolManager()));
